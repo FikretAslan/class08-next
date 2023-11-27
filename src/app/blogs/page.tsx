171cb3238247d4e58bb0d8f31a7b95413bc87329
Fikret Aslan <f.aslan0191@gmail.com>
2023-11-27 16:27:14 +0000

    rdy-for-project

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import {getPosts} from "."
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1> This is a cool blog page!</h1>
+    <p>My Blog Posts</p>
+    <ul>
+      {posts.map(post => {
+        return (
+          <li key={post.slug}></li>
+            <Link href={`/blogs/${post.slug}`}>{post.title}</Link>
+        )
+})}
+    </ul>
     </main>
+    
+    
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1> This is a cool blog page!</h1>
+      <h1 className="text-7xl font-bold text-white">Blogs R Us</h1>
     </main>
   );
 }
